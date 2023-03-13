32bf26732540a64c910b8050cee63b9fa2ba8fb4
rsof14 <sofia.rytik@gmail.com>
2023-03-13 18:18:16 +0300

    Сделано второе задание: панель администратора

---
+++ movies_admin/movies/models.py
@@ -0,0 +1,109 @@
+import uuid
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Этот параметр указывает Django, что этот класс не является представлением таблицы
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField(_('name'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"genre"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = _('Genre')
+        verbose_name_plural = _('Genres')
+
+
+class Gender(models.TextChoices):
+    MALE = 'male', _('male')
+    FEMALE = 'female', _('female')
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.CharField(_('full_name'), max_length=255)
+    gender = models.TextField(_('gender'), choices=Gender.choices, null=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"person"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = _('Person')
+        verbose_name_plural = _('Persons')
+
+
+class Filmwork(UUIDMixin, TimeStampedMixin):
+    title = models.CharField(_('name'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+    creation_date = models.DateField(_('creation_date'))
+    rating = models.FloatField(_('rating'), blank=True,
+                               validators=[MinValueValidator(0), MaxValueValidator(100)])
+    TYPE_CHOICES = (("MOVIE", _("Movie")), ("TV SHOW", _("TV Show")))
+    type = models.CharField(_('type'), max_length=10, choices=TYPE_CHOICES, default="MOVIE")
+    genres = models.ManyToManyField(Genre, through='GenreFilmwork')
+    persons = models.ManyToManyField(Person, through='PersonFilmwork')
+    certificate = models.CharField(_('certificate'), max_length=512, blank=True)
+    file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"film_work"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = _('Filmwork')
+        verbose_name_plural = _('Filmworks')
+
+
+class GenreFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(_("genre"))
+
+    class Meta:
+        db_table = "content\".\"genre_film_work"
+        verbose_name = _('Genre in filmwork')
+        verbose_name_plural = _('Genres in filmwork')
+
+
+
+class PersonFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.TextField(_('role'), null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(_("person"))
+
+    class Meta:
+        db_table = "content\".\"person_film_work"
+        verbose_name = _('Person in filmwork')
+        verbose_name_plural = _('Persons in filmwork')
